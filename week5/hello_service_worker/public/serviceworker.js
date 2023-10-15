f735dfad0b5d6236b95ae159b6236a2dcd458aaf
WeinanWanggit <114651132+WeinanWanggit@users.noreply.github.com>
2023-10-15 15:30:07 +0100

    remove node_moduls

--- week5/hello_service_worker/public/serviceworker.js
+++ week5/hello_service_worker/public/serviceworker.js
@@ -1,26 +1,37 @@
 console.log("Service Worker JavaScript called...")
+
+
+
 self.addEventListener("install", (ev)=>{
-    ev.waitUntil(caches.open('core')).then((cache)=>{
-        cache.add(new Request("cached.html")).then(
-            ()=>{console.log("installed cache...")}
-        )
-    })
-})
+    ev.waitUntil(
+        caches.open('core')
+            .then((cache) => {
+                return cache.add(new Request("test.ejs"));
+            })
+            .then(() => {
+                console.log("Cache installed successfully");
+            })
+            .catch((err) => {
+                console.error("Failed to install cache:", err);
+            })
+    );
+});
+
 
 
 self.addEventListener("activate", (ev)=>{
     console.log("activate")
 })
 
-self.addEventListener("fetch", (ev)=>{
-    const req = ev.request
-    if(req.headers.get("Accept").includes("text/html")){
-        ev.respondWith(
-            fetch(req).then((resp)=>{
-                return resp
-            }).catch((err)=>{
-                return caches.match("cached.html")
-            })
-        )
-    }
-})
+// self.addEventListener("fetch", (ev)=>{
+//     const req = ev.request
+//     if(req.headers.get("Accept").includes("text/html")){
+//         ev.respondWith(
+//             fetch(req).then((resp)=>{
+//                 return resp
+//             }).catch((err)=>{
+//                 return caches.match("cached.html")
+//             })
+//         )
+//     }
+// })
